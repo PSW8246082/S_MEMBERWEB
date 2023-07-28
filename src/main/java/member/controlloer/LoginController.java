3efc71273d97dfec538995661c6f3a51879202c3
parkseonwoo <emer8246082@gmail.com>
2023-07-28 18:13:08 +0900

    [ADD] 23/07/28 회원 로그인, 로그아웃 기능 추가
    회원 로그인, 로그아웃 컨트롤러 추가

---
+++ src/main/java/member/controlloer/LoginController.java
@@ -0,0 +1,72 @@
+package member.controlloer;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import member.model.service.MemberService;
+import member.model.vo.Member;
+
+/**
+ * Servlet implementation class LoginController
+ */
+@WebServlet("/member/login.do")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String memberId = request.getParameter("member-id");
+		String memberPw = request.getParameter("member-pw");
+		
+		//두개의 변수를 하나의 변수로 이용할수있게 객체만들기 - Member에서 미리 생성자 만들어야함
+		Member member = new Member(memberId, memberPw);
+		
+		//SELECT * FROM MEMBER_TBL WHERE MEMBER_ID = ? AND MEMBER_PW = ?
+		//int, member, List 중에 뭐로 받을지 생각해보기
+		MemberService service = new MemberService();
+		Member mOne = service.selectCheckLogin(member);
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("memberId", mOne.getMemberId());
+		session.setAttribute("memberName", mOne.getMemberName());
+		
+		if(mOne != null) {
+			//로그인 성공
+			request.setAttribute("msg", "로그인 성공");
+			request.setAttribute("url", "/index.jsp");
+			RequestDispatcher view = request.getRequestDispatcher("/member/serviceSuccess.jsp");
+			view.forward(request, response);   //누락 주의
+		} else {
+			//로그인 실패
+			request.setAttribute("msg", "로그인 실패");
+			RequestDispatcher view = request.getRequestDispatcher("/member/serviceFail.jsp");
+			view.forward(request, response);   //누락 주의
+		}
+	}
+
+}
